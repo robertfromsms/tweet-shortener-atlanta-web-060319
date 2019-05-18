07cc22c5ea1c82405a475a8dc8ff93fd38956272
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-18 01:48:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,3 +27,19 @@ def word_substituter(string)
   }
   string_array.join(" ")
 end
+
+def bulk_tweet_shortener(array_string)
+  array_string.each {|string|
+    index = 0
+    string_array = string.split(" ")
+    string_array.collect {|word|
+      dictionary.each {|key, value|
+        if word == key
+          string_array[index] = value
+        end
+      }
+      index += 1
+    }
+    puts string_array.join(" ")
+  }
+end
