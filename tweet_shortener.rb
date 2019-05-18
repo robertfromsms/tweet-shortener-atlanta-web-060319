4e5e63d79808833aacdfba76c2696c872fa16422
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-18 01:41:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,12 +4,12 @@ def dictionary
     "hello" => 'hi'
     "to" => "2"
     "two" => "2"
-    "too" => "2" 
+    "too" => "2"
     "for" => "4"
     "four" => "4"
     "be" => "b"
     "you" => "u"
-    "at" => "@" 
+    "at" => "@"
     "and" => "&"
   }
 end
