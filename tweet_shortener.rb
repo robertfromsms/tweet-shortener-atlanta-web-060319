0957fb23064135c7872de17fe722c071fd65c86b
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-18 01:41:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi'
+    "to" => "2"
+    "two" => "2"
+    "too" => "2" 
+    "for" => "4"
+    "four" => "4"
+    "be" => "b"
+    "you" => "u"
+    "at" => "@" 
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  index = 0
+  string_array = string.split(" ")
+  string_array.collect {|word|
+    dictionary.each {|key, value|
+      if word == key
+        string_array[index] = value
+    }
+    index += 1
+  }
+  string_array.join(" ")
+end
